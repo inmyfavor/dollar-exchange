2038d3065e15b03ea20186b7af785d219f33d874
inmyfavor <coolkidoriginal@icloud.com>
2023-04-11 12:56:44 +0300

    Initial Commit

---
+++ dollar/dollarApp.swift
@@ -0,0 +1,17 @@
+//
+//  dollarApp.swift
+//  dollar
+//
+//  Created by Ева Фельдман on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct dollarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
